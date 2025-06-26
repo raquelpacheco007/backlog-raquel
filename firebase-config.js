f79487db8de5c44da055db9a6050f8976acdd5bc
raquelpacheco007 <raquel.p.pereira@gmail.com>
2025-06-26 15:37:48 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxWtLwSZJ39JSD7188TfvzYrDwUH2dBvg",
+  authDomain: "backlog-e73b7.firebaseapp.com",
+  databaseURL: "https://backlog-e73b7-default-rtdb.firebaseio.com",
+  projectId: "backlog-e73b7",
+  storageBucket: "backlog-e73b7.firebasestorage.app",
+  messagingSenderId: "809930430331",
+  appId: "1:809930430331:web:84dafc933e414851c78fde",
+  measurementId: "G-VS6K931TH6"
+};
+
+// Disponibilizar globalmente
+window.firebaseConfig = firebaseConfig;
